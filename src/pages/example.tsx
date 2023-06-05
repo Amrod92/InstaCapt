4781d4a9ff571560147e318460695dde83b0af8a
Manlio Chite` <manliochite@amrod-MacBook-Pro.local>
2023-06-05 09:28:23 +0100

    Navbar pricing page and signin button add, layout page created, price page

--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -1,22 +1,19 @@
 // pages/example.tsx
-import { UserButton, useUser } from "@clerk/nextjs";
-import { SignInButton } from "@clerk/nextjs";
+import { UserButton, useUser } from '@clerk/nextjs'
+import { SignInButton } from '@clerk/nextjs'
 
 export default function Example() {
-
-  const user = useUser();
+  const user = useUser()
   return (
     <>
       <div>
         Your pages content can go here.
-        {
-          !user.isSignedIn && (<SignInButton mode="modal">
-          <button className="btn">Sign in</button>
-        </SignInButton>)
-
-        }
-        
+        {!user.isSignedIn && (
+          <SignInButton mode="modal">
+            <button className="btn">Sign in</button>
+          </SignInButton>
+        )}
       </div>
     </>
-  );
+  )
 }
